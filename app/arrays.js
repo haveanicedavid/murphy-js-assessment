316defc41ee4ca673a3f3d6a1bece8918881fe94
David Daniel <davedan7@gmail.com>
2015-09-03 13:10:41 -0600

    Working through arrays. Duplicates should be able to be refactored

--- app/arrays.js
+++ app/arrays.js
@@ -15,45 +15,75 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-    return arr.filter(function(x) {
-      return x !== item;
+    return _.reject(arr, function(x) {
+      return x === item;
     });
+    // return arr.filter(function(x) {
+    //   return x !== item;
+    // });
   },
 
   removeWithoutCopy : function(arr, item) {
+    for (var i = 0, length = arr.length; i < length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
 
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    var i, count = 0;
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        count++;
+      }
+    }
+    return count;
   },
 
   duplicates : function(arr) {
-
+    // var unique = _.uniq(arr);
+    var i, unique = [], result = [];
+    for (i = 0; i < arr.length; i++) {
+      if (unique.indexOf(arr[i]) < 0) {
+        unique.push(arr[i]);
+      } else {
+        result.push(arr[i]);
+      }
+    }
+    return _.uniq(result);
   },
 
   square : function(arr) {
--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -27,7 +27,7 @@ describe('arrays', function() {
     expect(result.join(' ')).to.eql('1 3 4');
   });
 
-  xit('you should be able to remove all instances of a value from an array, returning the original array', function() {
+  it('you should be able to remove all instances of a value from an array, returning the original array', function() {
     a.splice( 1, 0, 2 );
     a.push( 2 );
     a.push( 2 );
@@ -41,35 +41,35 @@ describe('arrays', function() {
     expect(result).equal(a);
   });
 
-  xit('you should be able to add an item to the end of an array', function() {
+  it('you should be able to add an item to the end of an array', function() {
     var result = arraysAnswers.append(a, 10);
 
     expect(result).to.have.length(5);
     expect(result[result.length - 1]).to.eql(10);
   });
 
-  xit('you should be able to remove the last item of an array', function() {
+  it('you should be able to remove the last item of an array', function() {
     var result = arraysAnswers.truncate(a);
 
     expect(result).to.have.length(3);
     expect(result.join(' ')).to.eql('1 2 3');
   });
 
-  xit('you should be able to add an item to the beginning of an array', function () {
+  it('you should be able to add an item to the beginning of an array', function () {
     var result = arraysAnswers.prepend(a, 10);
 
     expect(result).to.have.length(5);
     expect(result[0]).to.eql(10);
   });
 
-  xit('you should be able to remove the first item of an array', function () {
+  it('you should be able to remove the first item of an array', function () {
     var result = arraysAnswers.curtail(a);
 
     expect(result).to.have.length(3);
     expect(result.join(' ')).to.eql('2 3 4');
   });
 
-  xit('you should be able to join together two arrays', function() {
+  it('you should be able to join together two arrays', function() {
     var c = [ 'a', 'b', 'c', 1 ];
     var result = arraysAnswers.concat(a, c);
 
@@ -77,20 +77,20 @@ describe('arrays', function() {
     expect(result.join(' ')).to.eql('1 2 3 4 a b c 1');
   });
 
-  xit('you should be able to add an item anywhere in an array', function() {
+  it('you should be able to add an item anywhere in an array', function() {
     var result = arraysAnswers.insert(a, 'z', 2);
 
     expect(result).to.have.length(5);
     expect(result.join(' ')).to.eql('1 2 z 3 4');
   });
 
-  xit('you should be able to count the occurences of an item in an array', function() {
+  it('you should be able to count the occurences of an item in an array', function() {
     var result = arraysAnswers.count([ 1, 2, 4, 4, 3, 4, 3 ], 4);
 
     expect(result).to.eql(3);
   });
 
-  xit('you should be able to find duplicates in an array', function() {
+  it('you should be able to find duplicates in an array', function() {
     var result = arraysAnswers.duplicates([ 1, 2, 4, 4, 3, 3, 1, 5, 3 ]);
 
     expect(result.sort()).to.eql([1, 3, 4]);
