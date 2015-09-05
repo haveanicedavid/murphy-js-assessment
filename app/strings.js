5338d0afe71bc4ab94e2b664c004e44d717d2746
David Daniel <davedan7@gmail.com>
2015-09-05 12:46:09 -0600

    String passes first test, but need to change approach

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var letterCount = {};
+    var strArr = str.split('');
+    var result = [];
 
+    for (var i = 0; i < strArr.length; i++ ) {
+      if (! letterCount[strArr[i]]) {
+        letterCount[strArr[i]] = 1;
+      } else if (letterCount[strArr[i]] && letterCount[strArr[i]] < amount) {
+        letterCount[strArr[i]]++;
+      }
+    }
+
+    for (var letter in letterCount) {
+      if (letterCount.hasOwnProperty(letter)) {
+        _.times(letterCount[letter], function() {
+          result.push(letter);
+        });
+      }
+    }
+
+    return result.join('');
   },
   wordWrap: function(str, cols) {
 
